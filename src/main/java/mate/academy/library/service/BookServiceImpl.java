561a5a7813dbc54a76eaf09fa3a187580a9c3a60
Dmytro  Babenko <dima@Dmytros-MacBook-Air.local>
2026-05-07 12:23:03 +0800

    used Lombok for constructor, getters and setters

--- src/main/java/mate/academy/library/service/BookServiceImpl.java
+++ src/main/java/mate/academy/library/service/BookServiceImpl.java
@@ -1,17 +1,15 @@
 package mate.academy.library.service;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import mate.academy.library.dao.BookRepository;
 import mate.academy.library.model.Book;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
-
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
     @Override
     public Book save(Book book) {
