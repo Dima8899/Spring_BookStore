85e5e0b5622b8d94ac032220765dee168f09afcd
Dmytro  Babenko <dima@Dmytros-MacBook-Air.local>
2026-05-28 20:25:50 +0800

    fixed

--- src/main/java/mate/academy/library/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/library/dto/CreateBookRequestDto.java
@@ -3,6 +3,7 @@ package mate.academy.library.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
 
@@ -15,11 +16,11 @@ public class CreateBookRequestDto {
     @NotBlank(message = "Author cannot be null")
     private String author;
 
-    @NotNull(message = "ISBN cannot be null")
+    @NotBlank(message = "ISBN cannot be null")
     private String isbn;
 
     @NotNull(message = "Price cannot be null")
-    @Min(value = 0, message = "Price must be grater than 0")
+    @Positive(message = "Price must be grater than 0")
     private BigDecimal price;
     private String description;
     private String coverImage;
